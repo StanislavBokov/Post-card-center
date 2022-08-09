9fade3900bb30ed5ec98cae41934306190ff6bbf
StanislavBudin <zerrozauros96@list.ru>
2022-08-09 21:44:23 +0300

    add background store

--- src/store-provider/StoreContext.ts
+++ src/store-provider/StoreContext.ts
@@ -3,7 +3,8 @@ import {mainStore} from "../store";
 
 export const StoreContext = createContext<typeof mainStore>({
     modal: {activeModal: null, setModal: () => {}},
-    text: {value: '', setValue: () => {}}
+    text: {value: '', setValue: () => {}},
+    background: {colorState: '', setBackground: () => {}}
 })
 export const useStoreContext = () => {
     return useContext(StoreContext)
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,17 @@
 import {modalStore} from "./modal-store";
 import {textStore} from "./test-store";
-
+import { backgroundStore } from "./background-store";
 import {makeObservable, observable} from "mobx";
 
 class MainStore {
     modal = modalStore
     text = textStore
+    background = backgroundStore
     constructor() {
         makeObservable(this, {
             modal: observable.ref,
-            text: observable.ref
+            text: observable.ref,
+            background: observable.ref
         })
     }
 }
